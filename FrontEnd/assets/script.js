22b42417777957cae014757e843b7075559ca62f
Rébecca Andreze-Louison <al.rebecca@hotmail.fr>
2024-04-30 18:47:07 +0200

    Projet corrigé et terminé

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -37,7 +37,7 @@ function gestionConnexionUtilisateur() {
   const loginLogoutButton = document.getElementById("login-logout");
   const categoryFilters = document.getElementById("category-filters");
 
-  if (authToken) {
+  if (authToken) { // Si un token d'authentification est stocké dans le sessionStorage, l'utilisateur est connecté
     categoryFilters.style.display = "none";
     modifierButton.style.display = "block";
     loginLogoutButton.textContent = "Déconnecter";
@@ -45,7 +45,7 @@ function gestionConnexionUtilisateur() {
       sessionStorage.removeItem("authToken");
       window.location.reload();
     };
-  } else {
+  } else { // Si aucun token d'authentification n'est stocké dans le sessionStorage, l'utilisateur est déconnecté
     modifierButton.style.display = "none";
     loginLogoutButton.textContent = "Connexion";
     loginLogoutButton.onclick = function () {
@@ -88,13 +88,10 @@ function configurerModale() {
   const ouvrirModaleBouton = document.getElementById("open-modal");
   const fermerBouton = document.querySelector(".close-button");
 
-  // Ouvrir la modale en cliquant sur le bouton "Modifier"
   ouvrirModaleBouton.addEventListener(
     "click",
     () => (modale.style.display = "block")
   );
-
-  // Fermer la modale en cliquant sur le bouton de fermeture ou en dehors de la modale (sur le fond)
   fermerBouton.addEventListener("click", () => (modale.style.display = "none"));
   modale.addEventListener("click", (evenement) => {
     if (evenement.target === modale) modale.style.display = "none";
@@ -103,9 +100,10 @@ function configurerModale() {
 
 // Affiche les projets dans la modale et gère la suppression sans rechargement de la page
 function afficherProjetsDansModale(projects) {
-  let conteneurModal = document.querySelector("#modal-projects-container");
-  conteneurModal.innerHTML = projects
+  let conteneurModal = document.querySelector("#modal-projects-container"); 
+  conteneurModal.innerHTML = projects 
     .map(
+    
       (projet) => `
     <figure class="project-figure" data-id="${projet.id}">
       <img src="${projet.imageUrl}" alt="${projet.title}">
@@ -115,40 +113,50 @@ function afficherProjetsDansModale(projects) {
     </figure>
   `
     )
-    .join("");
-
-  // Ajoute des écouteurs d'événements pour la suppression de projets dans la modale
+    .join(""); 
+  // Ajoute un écouteur d'événements pour chaque icône de corbeille pour supprimer un projet en cliquant sur l'icône de corbeille
   conteneurModal.querySelectorAll(".fa-trash-can").forEach((icon) => {
+   
     icon.addEventListener("click", function (event) {
-      supprimerProjet(icon.getAttribute("data-id"), event); // Passez l'événement ici
-    });
+      
+      supprimerProjet(icon.getAttribute("data-id"), event); 
+    }); 
   });
 }
 
-// Supprime un projet en utilisant l'API sans recharger la page et met à jour les projets affichés dans la galerie et la modale
+// Supprime un projet sans rechargement de la page après la suppression du projet dans la galerie et la modale
 async function supprimerProjet(projectId, event) {
-  event.preventDefault();
   event.stopPropagation();
+
   try {
+    console.log("Tentative de suppression du projet avec l'ID :", projectId);
+
     const reponse = await fetch(
       `http://localhost:5678/api/works/${projectId}`,
       {
         method: "DELETE",
         headers: {
+          "Content-Type": "application/json",
           Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
         },
       }
     );
-    if (!reponse.ok) throw new Error("Problème avec la réponse de l'API");
 
-    // Mettre à jour la liste des projets affichés dans la galerie et la modale après la suppression du projet
-    // tousLesProjets = tousLesProjets.filter((projet) => projet.id !== projectId);
-    // afficherProjets(tousLesProjets);
-    // afficherProjetsDansModale(tousLesProjets);
+    if (!reponse.ok)
+      throw new Error(`Erreur lors de la suppression: ${reponse.statusText}`);
 
-    // Garder la modale ouverte après la suppression du projet
-    const modal = document.getElementById("modal");
-    modal.style.display = "block";
+    console.log(
+      "Le projet avec l'ID",
+      projectId,
+      "a été supprimé avec succès."
+    );
+
+    // Supprime l'élément du DOM correspondant au projet supprimé de la galerie
+    document.querySelector(`figure[data-id="${projectId}"]`).remove();
+    document.querySelector(`.project-figure[data-id="${projectId}"]`).remove();
+
+    // Mettre à jour l'array tousLesProjets après la suppression
+    tousLesProjets = tousLesProjets.filter((projet) => projet.id !== projectId);
   } catch (erreur) {
     console.error("Erreur lors de la suppression du projet :", erreur);
   }
@@ -161,10 +169,9 @@ function configurerNavigationModale() {
     "#modal-add-photo-view .fa-arrow-left" // Sélectionne l'icône flèche gauche dans la vue d'ajout de photo de la modale
   );
 
-  // Affiche la vue d'ajout de photo lors du clic sur le bouton "Ajouter une photo"
   boutonAjoutPhoto.addEventListener("click", () =>
     basculerVuesModale("addPhoto")
-  ); // Retourne à la vue de la galerie lors du clic sur le bouton de retour
+  );
   boutonRetour.addEventListener("click", () => basculerVuesModale("gallery"));
 }
 
@@ -172,8 +179,8 @@ function configurerNavigationModale() {
 function basculerVuesModale(vue) {
   const vueGalerie = document.getElementById("modal-gallery-view");
   const vueAjoutPhoto = document.getElementById("modal-add-photo-view");
-  vueGalerie.style.display = vue === "addPhoto" ? "none" : "flex"; // Cache la vue de la galerie si la vue d'ajout de photo est active
-  vueAjoutPhoto.style.display = vue === "addPhoto" ? "block" : "none"; // Cache la vue d'ajout de photo si la vue de la galerie est active
+  vueGalerie.style.display = vue === "addPhoto" ? "none" : "flex";
+  vueAjoutPhoto.style.display = vue === "addPhoto" ? "block" : "none";
 }
 
 // Récupère et affiche les catégories depuis l'API
@@ -199,7 +206,6 @@ async function ajouterProjetViaFormulaire() {
   const formulaire = document.getElementById("photoAddForm");
   formulaire.addEventListener("submit", async (event) => {
     event.preventDefault();
-    event.stopPropagation();
     const formData = new FormData(formulaire);
     try {
       const reponse = await fetch("http://localhost:5678/api/works", {
@@ -216,10 +222,28 @@ async function ajouterProjetViaFormulaire() {
       afficherProjets(tousLesProjets);
       afficherProjetsDansModale(tousLesProjets);
       basculerVuesModale("gallery");
+      reinitialiserFormulaireAjoutImage(); // Réinitialise le formulaire après l'ajout
     } catch (erreur) {
       console.error("Erreur lors de l'ajout du projet :", erreur);
     }
   });
+}
+
+// Fonction pour réinitialiser le formulaire d'ajout d'image
+function reinitialiserFormulaireAjoutImage() {
+  const formulaire = document.getElementById("photoAddForm");
+  const previewImg = document.querySelector("#addPhotoForm img.image-preview");
+  const labelAjouterPhoto = document.querySelector(
+    "#addPhotoForm label.AjouterPhoto"
+  );
+  const iconImage = document.querySelector("#addPhotoForm .fa-image");
+
+  // Réinitialise le formulaire après l'ajout
+  formulaire.reset();
+  previewImg.src = "#";
+  previewImg.style.display = "none";
+  labelAjouterPhoto.style.display = "block";
+  iconImage.style.display = "block";
 }
 
 // Initialise la prévisualisation d'image et vérifie l'état du formulaire
@@ -236,52 +260,53 @@ function initialiserPrevisualisationImageEtVerifierFormulaire() {
     "#photoAddForm button[type='submit']"
   );
 
-  // Écouteur d'événement pour afficher l'image sélectionnée dans le formulaire d'ajout de photo et masquer le label et l'icône par défaut du formulaire d'ajout de photo lors de la sélection d'une image à télécharger
+  // Affiche l'image sélectionnée dans le formulaire d'ajout de photo en tant que prévisualisation de l'image avant l'ajout du projet
   inputFile.addEventListener("change", function () {
     const fichier = inputFile.files[0];
     if (fichier) {
+      // Vérifie si un fichier est sélectionné
       const reader = new FileReader();
       reader.onload = function (e) {
         previewImg.src = e.target.result;
         previewImg.style.display = "block";
-        labelAjouterPhoto.style.display = "none"; // Cache le label
-        iconImage.style.display = "none"; // Cache l'icône
+        labelAjouterPhoto.style.display = "none";
+        iconImage.style.display = "none";
         checkFormAndToggleSubmitButton(); // Vérifie l'état du formulaire après le changement
       };
-      reader.readAsDataURL(fichier); // Lit le contenu du fichier en tant qu'URL de données
+      reader.readAsDataURL(fichier);
     } else {
-      // Réinitialise l'image de prévisualisation et affiche le label et l'icône par défaut du formulaire d'ajout de photo
+      // Réinitialise le formulaire si aucun fichier n'est sélectionné
       previewImg.style.display = "none";
       labelAjouterPhoto.style.display = "block";
       iconImage.style.display = "block";
-      checkFormAndToggleSubmitButton(); // Vérifie l'état du formulaire après le changement
+      checkFormAndToggleSubmitButton();
     }
   });
 
-  // Fonction pour vérifier l'état du formulaire et activer/désactiver le bouton "Valider" en fonction de l'état du formulaire
+  // Vérifie l'état du formulaire et active/désactive le bouton de soumission en fonction de l'état du formulaire (rempli ou non)
   function checkFormAndToggleSubmitButton() {
     if (
       titleInput.value.trim() &&
       inputFile.files.length > 0 &&
       categorySelect.value
     ) {
-      submitButton.style.backgroundColor = "#1D6154"; // Couleur lorsque le formulaire est valide
+      submitButton.style.backgroundColor = "#1D6154";
       submitButton.disabled = false;
     } else {
-      submitButton.style.backgroundColor = "#a7a7a7"; // Couleur lorsque le formulaire n'est pas valide
+      submitButton.style.backgroundColor = "#a7a7a7";
       submitButton.disabled = true;
     }
   }
 
-  // Écouteurs d'événements pour vérifier l'état du formulaire à chaque changement
+  // Ajoute des écouteurs d'événements pour les champs du formulaire pour vérifier l'état du formulaire après chaque modification du champ
   titleInput.addEventListener("input", checkFormAndToggleSubmitButton);
   categorySelect.addEventListener("change", checkFormAndToggleSubmitButton);
   inputFile.addEventListener("change", checkFormAndToggleSubmitButton);
 
-  // Appel initial à la fonction pour définir l'état initial du bouton "Valider"
   checkFormAndToggleSubmitButton();
 }
 
+// Exécute le code JavaScript après le chargement du DOM
 document.addEventListener("DOMContentLoaded", async () => {
   await recupererEtAfficherProjets();
   configurerBoutonsFiltre();
